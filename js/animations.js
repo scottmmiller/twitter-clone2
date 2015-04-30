d12073136f3b6abdcbc13e1fcf48c1127c705cc1
Scott M Miller <smmiller.work@gmail.com>
2015-04-29 19:06:04 -0600

    got step 4 (button disable/enable) working

--- js/animations.js
+++ js/animations.js
@@ -4,35 +4,40 @@ $(document).ready(function() {
     
     $('#tweet-controls').hide();                  //STEP 1
     
-    $('.tweet-compose').on('click', function() {    //STEP 2
+    $('.tweet-compose').on('focus', function() {    //STEP 2
         $(this).animate({height: '5em'});
         $('#tweet-controls').show();
     });
     
-//    $('.tweet-compose').off('click', function() {
-//        $(this).animate({height: '2.5em'});
-//        $('#tweet-controls').hide();
-//    });    
-//    
+    $('.tweet-compose').on('blur', function() {    //STEP 2
+        $(this).animate({height: '2.5em'});
+        $('#tweet-controls').hide();
+    });    
+    
     $('.tweet-compose').keyup(function() {      //STEP 3
             console.log('tweet-compose.length', $('.tweet-compose').val().length);
-        var count = 20-$('.tweet-compose').val().length;
-             console.log('Count: ', count);
-        $('#char-count').html(count);
-        if(count <= 10) {
+        var charCount = 20-$('.tweet-compose').val().length;
+             console.log('charCount: ', charCount);
+        $('#char-count').html(charCount);
+        if(charCount <= 10) {
             $('#char-count').css('color', 'red');
         }
         else {
             $('#char-count').css('color', 'inherit')
-        }       
-        if(count < 0 ) {                        //STEP 4
-            $('#tweet-submit .button').prop('disabled', true);
+        }
+        
+        if(charCount >= 140 || charCount < 0) {          //STEP 4
+            $('#tweet-submit').attr('disabled', true);
         }
         else {
-            $('#tweet-submit .button').prop('disabled', false);
+            $('#tweet-submit').removeAttr('disabled');
         }
     });
     
+    $('#tweet-submit .button').click(function() {
+        var tweetText = $('.tweet:first').clone();
+        
+    });
     
     
 });
